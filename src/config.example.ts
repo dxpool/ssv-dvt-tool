130e5771bfce5a0bbc48f82c9dd30351cfd6986b
frankye23 <dafeng.ye@intchains.com>
2025-04-29 16:52:47 +0800

    feat: update config.example

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,45 +1,19 @@
-import { Config } from './types.config';
+import { Config } from "./types.config";
 
-// 1. create a config.ts file in the same category 
-// 2. copy the code below 
-// 3. fill the blanks (nodeUrl, ownerAddress, defaultOperator(optional), externalLink, baseUrl)
-
-export default <Config> {
-  network:{
-    holesky: {
-      nodeUrl: "",
-      network: "holesky",
-      ownerAddress: "",
-      defaultOperator: [
-        {
-          name: "",
-          id: 1,
-          fee: "",
-          validators_count: 1,
-          mev_relays: "",
-          logo: "",
-          public_key: ""
-        }
-      ]
-    },
+export default <Config>{
+  network: {
     mainnet: {
       nodeUrl: "",
       network: "mainnet",
       ownerAddress: "",
-      defaultOperator: [
-        {
-          name: "",
-          id: 1,
-          fee: "",
-          validators_count: 1,
-          mev_relays: "",
-          logo: "",
-          public_key: ""
-        }
-      ]
+    },
+    hoodi: {
+      nodeUrl: "",
+      network: "hoodi",
+      ownerAddress: "",
     },
   },
   externalLink: "https://stake.dxpool.com/ethereum/staking?step=1",
   // This is our api to get the operator list, if you want to use ssv official api, you may need to change specific code in buildUrl function
   baseUrl: "https://ssv.stake.dxpool.in/api/v1/validators/eth-ssv-operator"
-}
+};
